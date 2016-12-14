1430220ba4fb3ee57f0956193ed5411a2391356a
Logan Gingerich <logangingerich@yahoo.com>
2016-12-14 08:39:03 -0700

    Assignment 17: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,22 @@ posts = Post.all
   )
 end
 
+# Create Unique Post
+puts "#{Post.count}"
+unique_post = Post.find_or_create_by(
+  title: "GREETINGS",
+  body: "Hello, World!"
+)
+puts "#{Post.count}"
+
+# Create Unique Post
+puts "#{Comment.count}"
+Comment.find_or_create_by(
+  post: unique_post,
+  body: "Unique comment body."
+)
+puts "#{Comment.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
