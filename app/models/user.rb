8a3b6a60390efa1c8e2642219ecd0f2b8bb82f8f
Logan Gingerich <logangingerich@yahoo.com>
2016-12-25 08:51:31 -0700

    Checkpoint 27: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   before_save { self.email = email.downcase if email.present? }
   validates :name, length: {minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -9,5 +10,5 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
-            
+
 end
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,4 +1,5 @@
 module RandomData
+
   def self.random_paragraph
     sentences = []
     rand(4..6).times do
@@ -23,4 +24,15 @@ module RandomData
     letters.shuffle!
     letters[0,rand(3..8)].join
   end
+
+  def self.random_name
+    first_name = random_word.capitalize
+    last_name = random_word.capitalize
+    "#{first_name} #{last_name}"
+  end
+
+  def self.random_email
+    "#{random_word}@#{random_word}.#{random_word}"
+  end
+  
 end
