eaae2434258d90ae9c4a4b121c1820332127677e
Logan Gingerich <logangingerich@yahoo.com>
2017-01-03 09:35:23 -0700

    Checkpoint 30: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
+  
   validates :name, length: {minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
